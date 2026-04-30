fd1c61a53c730c7ba2739173862095b958c62d5a
Rehan Shaikh <rehandev3@gmail.com>
2026-05-01 01:48:18 +0530

    fix: update API base URLs for authentication and chat services to production environment

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -3,7 +3,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/auth",
+    baseURL: "https://perplexity-1-4jje.onrender.com/api/auth",
     withCredentials: true
 })
 
--- Frontend/src/features/chat/services/chat.api.js
+++ Frontend/src/features/chat/services/chat.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/chats",
+    baseURL: "https://perplexity-1-4jje.onrender.com/api/chats",
     withCredentials: true,
 })
 
