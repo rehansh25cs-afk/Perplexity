f79a0fb6dc4191e0394e39e8b2285baebd2ae388
Rehan Shaikh <rehandev3@gmail.com>
2026-05-09 12:17:13 +0530

    fix: update email transporter authentication method to use app password

--- Backend/src/services/mail.service.js
+++ Backend/src/services/mail.service.js
@@ -3,13 +3,10 @@ import nodemailer from "nodemailer"
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        type: 'OAuth2',
-        user: process.env.GOOGLE_EMAIL_USER,
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+      user: process.env.GOOGLE_EMAIL_USER,
+      pass: process.env.GOOGLE_APP_PASSWORD,
     },
-});
+  });
 
 transporter.verify()
     .then(() => {
